8a47776d3c6b89b990ca08994ed69bfd30ca2f03
darrendoyle37 <darren.doyle>
2021-05-01 16:14:07 +0100

    Full CRUD Functionationality working

---
+++ Frontend/FifaFinderWebApp/src/app/post/post-display/post-display.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-post-display',
+  templateUrl: './post-display.component.html',
+  styleUrls: ['./post-display.component.css']
+})
+export class PostDisplayComponent implements OnInit {
+
+  constructor(private service: SharedService) { }
+
+  PostList: any = [];
+
+  ModalTitle: string;
+  ShowPostOperations: boolean = false;
+  post: any;
+
+  ngOnInit(): void {
+    this.refreshPostList();
+  }
+
+  refreshPostList() {
+    this.service.getPostList().subscribe(data => {
+      this.PostList = data;
+    });
+  }
+
+  addClick() {
+    this.post = {
+      ID: 0,
+      Type: "",
+      Platform: "",
+      Position: "",
+      PlayerRating: 80,
+      Description: "",
+    }
+    this.ModalTitle = "Add Post";
+    this.ShowPostOperations = true;
+  }
+  closeClick() {
+    this.ShowPostOperations = false;
+    this.refreshPostList();
+  }
+
+  editPostClick(item) {
+    console.log(item);
+    this.post = item;
+    this.post
+    this.ModalTitle = "Edit Post";
+    this.ShowPostOperations = true;
+  }
+
+  deletePostClick(item) {
+    if (confirm('Are you sure you want to delete this post? This action is irreversible.')) {
+      console.log(item.ID);
+      this.service.deletePost(item.ID).subscribe(data => {
+        alert(data.toString());
+        this.refreshPostList();
+      })
+    }
+  }
+
+
+
+}
