bbf6cf4d81afc32eef63d9bb6e6daad5379c252a
darrendoyle37 <darren.doyle>
2021-05-07 14:40:49 +0100

    Initial Repository Setup

--- Backend/FifaFinderAPI/FifaFinderTest/UserController.cs
+++ Backend/FifaFinderAPI/FifaFinderTest/UserController.cs
@@ -1,44 +1,44 @@
-//using FifaFinderAPI.Controllers;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using Xunit;
+using FifaFinderAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
 
-//namespace FifaFinderTest
-//{
-//    public class UserControllerTest
-//    {
-//        private UserController userController;
+namespace FifaFinderTest
+{
+    public class UserControllerTest
+    {
+        private UserController userController;
 
-//        [Fact]
-//        public void GetUsers_Test()
-//        {
-//            // arrange
+        [Fact]
+        public void GetUsers_Test()
+        {
+            // arrange
 
-//            // act 
-//            var controllerActionResult = userController.GetUsers();
-//            // assert
-//            Assert.Equal("User table has no records", controllerActionResult.ToString());
-//        }
-//        [Fact]
-//        public void LoginUser_Test()
-//        {
-//            // arrange
-//            // act 
-//            // assert
-//        }
-//        [Fact]
-//        public void RegisterUser_Test()
-//        {
-//            // arrange
-//            // act 
-//            // assert
-//        }
-//        [Fact]
-//        public void DeleteUser_Test()
-//        {
-//            // arrange
-//            // act 
-//            // assert
-//        }
-//    }
-//}
+            // act 
+            var controllerActionResult = userController.GetUsers();
+            // assert
+            Assert.Equal("User table has no records", controllerActionResult.ToString());
+        }
+        [Fact]
+        public void LoginUser_Test()
+        {
+            // arrange
+            // act 
+            // assert
+        }
+        [Fact]
+        public void RegisterUser_Test()
+        {
+            // arrange
+            // act 
+            // assert
+        }
+        [Fact]
+        public void DeleteUser_Test()
+        {
+            // arrange
+            // act 
+            // assert
+        }
+    }
+}
